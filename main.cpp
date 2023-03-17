1981e1a8fe8a0c01397751f9527679f0f13181b4
muriel <tannemur@students.zhaw.ch>
2023-03-17 08:39:43 +0100

    end of workshop 1

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ bool do_reset_all_once = false;    // this variable is used to reset certain var
 // objects for user button (blue button) handling on nucleo board
 DebounceIn user_button(PC_13);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
 void user_button_pressed_fcn(); // custom functions which get executed when user button gets pressed, definition below
-
+float doMath(float number);
 
 // main runs as an own thread
 int main()
@@ -20,7 +20,7 @@ int main()
     user_button.fall(&user_button_pressed_fcn);
 
     // while loop gets executed every main_task_period_ms milliseconds (simple aproach to repeatedly execute main)
-    const int main_task_period_ms = 50; // define main task period time in ms e.g. 50 ms -> main task runs 20 times per second
+    const int main_task_period_ms = 1000; // define main task period time in ms e.g. 50 ms -> main task runs 20 times per second
     Timer main_task_timer;              // create Timer object which we use to run the main task every main_task_period_ms
 
 
@@ -33,12 +33,15 @@ int main()
 
     // mechanical button
     DigitalIn mechanical_button(PC_5); // create DigitalIn object to evaluate extra mechanical button, you need to specify the mode for proper usage, see below
-    mechanical_button.mode(PullUp);    // set pullup mode: sets pullup between pin and 3.3 V, so that there is a defined potential
+    mechanical_button.mode(PullUp);
+    //DigitalIn mechanical_button(PC_1);     // set pullup mode: sets pullup between pin and 3.3 V, so that there is a defined potential
+
 
 
     // Sharp GP2Y0A41SK0F, 4-40 cm IR Sensor
     float ir_distance_mV = 0.0f; // define variable to store measurement
     //??? // create AnalogIn object to read in infrared distance sensor, 0...3.3V are mapped to 0...1
+    AnalogIn ir_analog_in(PC_2);
 
 
     main_task_timer.start();
@@ -53,7 +56,7 @@ int main()
             if (mechanical_button.read()) {
 
                 // read analog input
-                //ir_distance_mV = ???;
+                ir_distance_mV = 1.0e3f * ir_analog_in.read() * 3.3f;
 
             }
 
@@ -74,8 +77,10 @@ int main()
         // toggling the user led
         user_led = !user_led;
 
-        // do only output via serial what's really necessary, this makes your code slow
+        // do only output via serial what's really necessary, this makes your code 
+        printf("im working");
         printf("IR sensor (mV): %3.3f\r\n", ir_distance_mV);
+        printf("test (cm): %3.3f\r\n", doMath(ir_distance_mV));
 
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
@@ -88,4 +93,10 @@ void user_button_pressed_fcn()
     // do_execute_main_task if the button was pressed
     do_execute_main_task = !do_execute_main_task;
     if (do_execute_main_task) do_reset_all_once = true;
+}
+    
+float doMath(float number){
+    float a = 1.255e+04;
+    float b = 24.7;
+    return a/(b+number);
 }
